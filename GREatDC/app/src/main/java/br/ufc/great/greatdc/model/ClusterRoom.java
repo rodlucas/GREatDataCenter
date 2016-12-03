738bb87de475bc07d4f2f91afd00f774f7a0bc3d
antoniorafaelbraga <rafaeltelematica@gmail.com>
2016-12-03 00:29:45 -0300

    Incluindo a classe ClusterRoom;

---
+++ GREatDC/app/src/main/java/br/ufc/great/greatdc/model/ClusterRoom.java
@@ -0,0 +1,28 @@
+package br.ufc.great.greatdc.model;
+
+/**
+ * Created by rafaelbraga on 03/12/16.
+ */
+
+public class ClusterRoom {
+
+    double temperature;
+    double humidity;
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+}
